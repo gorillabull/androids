469afdfcc33a2a57147800a58ac83dd308fab964
vasilevl <vasilevl@unlv.nevada.edu>
2017-09-28 22:31:35 -0700

    change2

---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var t = 5;
+            t++;
+            
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
